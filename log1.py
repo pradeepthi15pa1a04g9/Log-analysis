9e3367f461c24e6986a9e00a9d8f32c4baf24750
pradeepthi15pa1a04g9 <38848947+pradeepthi15pa1a04g9@users.noreply.github.com>
2018-06-07 10:39:59 +0530

    updated

--- log1.py
+++ log1.py
@@ -9,28 +9,28 @@ def popular_articles():
     print("THE POPULAR ARTICLES ARE:")
     cur.execute("select title,views from top_articles_views limit 3")
     result = cur.fetchall()
-    for i, value in enumerate(result):
-        print (str(result[i]) + "views")
+    for y in result:
+        print '"', y[0], '"', '-- ', y[1], " views"
 
 
 def popular_authors():
     print("THE POPULAR AUTHORS ARE:")
     cur.execute("select * from top_authors_views ")
     result = cur.fetchall()
-    for i, value in enumerate(result):
-        print (str(result[i]) + "views")
+    for z in result:
+        print z[0], ' -- ', z[1], " views"
 
 
 def error_percentage():
     print("THE ERROR '%' IS:")
-    cur.execute('''select to_char(day, 'Mon DD, YYYY') as day, percentage
-                  from errors_percent where percentage > 1.0 group by day,
-                  percentage order by percentage desc;''')
+    cur.execute(''' select to_char(day, 'Mon DD, YYYY') as day, percentage
+                   from errors_percent where percentage > 1.0 group by day,
+                   percentage order by percentage
+                   desc;
+                ''')
     result = cur.fetchall()
-    for i in range(len(result)):
-        date = result[i][0]
-        errors_percent = result[i][1]
-        print("%s--%.1f %%" % (date, errors_percent))
+    for date, errors_percent in result:
+        print('{} -- {:.1f}% errors'.format(date, errors_percent))
 popular_articles()
 popular_authors()
 error_percentage()
